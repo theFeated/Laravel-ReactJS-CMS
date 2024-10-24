35d0cdf39257cf3ec9be481784a40b4735ae6898
theFeated <jvpdionsay01667@usep.edu.ph>
2024-10-24 11:58:53 +0800

    ♻️ Refactor user table migration to remove unused columns

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,9 +20,6 @@ return new class extends Migration
             $table->string('userphoto')->nullable();
             $table->string('google_id')->nullable();
             $table->string('two_factor_code')->nullable();
-            $table->string('google2fa_secret')->nullable();
-            $table->boolean('google2fa_setup_complete')->default(false);
-            $table->boolean('google2fa_enabled')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
